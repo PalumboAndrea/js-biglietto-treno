fffa683e1b08a1c1b2703148e1ff750f372d1484
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-11-21 17:46:43 +0100

    update

--- js/myscript.js
+++ js/myscript.js
@@ -6,3 +6,24 @@
 //    - va applicato uno sconto del 40% per gli over 65.
 //  L'output del prezzo finale va emesso (presentato all'utente) in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 
+let priceEveryKm = '0.21';
+
+const kmToDo = prompt("Inserie i chilometri da percorrere");
+console.log(kmToDo);
+document.getElementById('km-to-do').innerHTML = 'Chilometri da effettuare: ' + kmToDo + 'km';
+
+const age = prompt("Inserie l'età del passeggero");
+console.log(age);
+document.getElementById('age').innerHTML = 'Età: ' + age + ' anni';
+
+let teensDiscount = 0.20;
+
+let overDiscount = 0.40;
+
+
+
+
+
+
+
+document.getElementById('price').innerHTML = 'Costo del biglietto: ' + kmToDo * priceEveryKm + '&euro;';
