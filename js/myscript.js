d6b348b58d1ff0d164360ca458bdf99c7b3543d6
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-11-21 18:34:06 +0100

    update

--- js/myscript.js
+++ js/myscript.js
@@ -6,24 +6,43 @@
 //    - va applicato uno sconto del 40% per gli over 65.
 //  L'output del prezzo finale va emesso (presentato all'utente) in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 
-let priceEveryKm = '0.21';
 
-const kmToDo = prompt("Inserie i chilometri da percorrere");
+const kmToDo = parseInt(prompt("Inserie i chilometri da percorrere"));
 console.log(kmToDo);
-document.getElementById('km-to-do').innerHTML = 'Chilometri da effettuare: ' + kmToDo + 'km';
+document.getElementById('km-to-do').innerHTML = 'Tratto da effettuare: ' + kmToDo + 'km;';
 
-const age = prompt("Inserie l'età del passeggero");
+let sexType;
+
+let sex = prompt("Inserie il sesso del passeggero/a (maschio o femmina)");
+console.log(sex);
+if (sex = "femmina"){
+    sexType = "della passeggera"
+} else {
+    sexType = "del passeggero"
+}
+
+const age = parseInt(prompt("Inserie l'età del passeggero"));
 console.log(age);
-document.getElementById('age').innerHTML = 'Età: ' + age + ' anni';
+if (age <= 1){
+    document.getElementById('age').innerHTML = 'Età ' + sexType + ': ' + age + ' anno;';
+} else {
+    document.getElementById('age').innerHTML = 'Età ' + sexType + ': ' + age + ' anni;';
+}
+
+let priceEveryKm = 0.21;
 
 let teensDiscount = 0.20;
 
 let overDiscount = 0.40;
 
+let priceAfterDiscount;
 
+if (age < 18){
+    priceAfterDiscount = (priceEveryKm - (priceEveryKm * teensDiscount));
+} else {
+    priceAfterDiscount = (priceEveryKm - (priceEveryKm * overDiscount));
+}
 
+console.log(priceAfterDiscount);
 
-
-
-
-document.getElementById('price').innerHTML = 'Costo del biglietto: ' + kmToDo * priceEveryKm + '&euro;';
+document.getElementById('price').innerHTML = 'Costo del biglietto: ' + kmToDo * priceAfterDiscount.toFixed(4) + '&euro;';
